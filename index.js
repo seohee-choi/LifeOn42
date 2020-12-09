5b95e7f6bab94121da4e660d81eb5951a2e8e08a
seohee <seohee@MacBook-Pro.local>
2020-12-09 22:06:03 +0900

    surveyInit create

---
+++ index.js
@@ -0,0 +1,23 @@
+const userForm = document.querySelector(".js-user");
+const userInput = document.querySelector(".js-id");
+const startBtn = document.querySelector(".js-button");
+// console.log(userInput.value);
+
+// function localName(event) {}
+
+function qnaStart() {
+  const userName = userInput.value;
+  const alertName = document.querySelector(".alertName");
+  if (userName < 1) {
+    alertName.innerText = "Write your name ☝";
+  }
+  console.log(userName);
+  surveyInit();
+  // location.href='survey/survey.html'
+}
+
+function init() {
+  startBtn.onclick = qnaStart;
+}
+
+init();
