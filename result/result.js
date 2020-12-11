f01e68144d5dad35e518ab8211e23078f8d040da
seohchoi <ume_feat@naver.com>
2020-12-11 18:47:14 +0900

    img file name changed

--- result/result.js
+++ result/result.js
@@ -1,9 +1,9 @@
-function paintUserName(userName){
-	
+function paintUserName(userName){	
 	const title = document.querySelector(".js-title");
 	title.innerText = `${userName}님의 LifeOn42는...`;
 	console.log(userName);
 }
+
 function getUserName(){
 	const userName = location.href.substr(location.href.lastIndexOf('=')+1);
 	if (userName)
